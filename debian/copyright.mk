4ac55677bef0bef5bf60c98e73303a6c59632c1e
Andrew Shadura <andrew.shadura@collabora.co.uk>
2016-02-21 12:53:38 +0100

    Unbreak copyright.mk

--- debian/copyright.mk
+++ debian/copyright.mk
@@ -1,5 +1,5 @@
 copyright-tools:
-	@which licensecheck2 /usr/lib/cdbs/li2censecheck2dep5 >/dev/null \
+	@which licensecheck /usr/lib/cdbs/licensecheck2dep5 >/dev/null \
 		|| { \
 			echo $(filter %-copyright,$(MAKECMDGOALS)) requires devscripts and cdbs >&2 ; \
 			false; \
@@ -12,5 +12,5 @@ gen-copyright: copyright-tools
 check-copyright: copyright-tools
 	@licensecheck --copyright -r `find * -type f` 2>/dev/null | \
 	  /usr/lib/cdbs/licensecheck2dep5 > debian/copyright.tmp
-	@diff -q -u debian/copyright.auto debian/copyright.tmp
+	@diff -u debian/copyright.auto debian/copyright.tmp
 	@rm -f debian/copyright.tmp
